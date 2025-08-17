1ab92bc8621c4a9f840627f7515b10b85d3d0705
Masrafi Iqbal <masrafiiqbal@gmail.com>
2025-08-17 08:54:14 +0600

    Profile page and sidebar added

---
+++ src/pages/Profile.js
@@ -0,0 +1,21 @@
+import React from "react";
+import profilePic from "../assets/profile.jpg"; 
+
+function Profile() {
+  return (
+    <div className="profile-page">
+      <h1>Profile</h1>
+     <img
+        src={profilePic}
+        alt="Profile"
+        className="profile-pic"
+        style={{ width: 220, height: 220, borderRadius: "50%" }}
+      />
+      <h2>Masrafi Iqbal</h2>
+      <p>Email: masrafi@gmail.com</p>
+      <p>Date Joined: January 15, 2024</p>
+    </div>
+  );
+}
+
+export default Profile;
