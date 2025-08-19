580bd0e5e9865dd4fe89372269d5a7feb5f1020f
Masrafi Iqbal <masrafiiqbal@gmail.com>
2025-08-20 01:29:59 +0600

    designed Profile page

--- src/pages/MyArticles.js
+++ src/pages/MyArticles.js
@@ -6,10 +6,11 @@ function MyArticles() {
 const allArticles = [
   { title: "AI-Powered Chatbots:\n    Enhancing User Experience with NLP", status: "Published", lastEdited: "2 days ago", category: "Artificial Intelligence" },
   { title: "Blockchain Technology:\n    Securing Digital Transactions", status: "Draft", lastEdited: "1 week ago", category: "Blockchain" },
+  { title: "Cloud Computing:\n    Optimizing Storage and Scalability", status: "Published", lastEdited: "2 months ago", category: "Cloud Computing" },
   { title: "IoT in Smart Cities:\n    Real-Time Data for Urban Planning", status: "Published", lastEdited: "2 weeks ago", category: "Internet of Things" },
   { title: "Machine Learning:\n   Predictive Analytics for Big Data", status: "Published", lastEdited: "3 weeks ago", category: "Machine Learning" },
   { title: "Cybersecurity Systems:\n    Threat Detection Using AI", status: "Draft", lastEdited: "1 month ago", category: "Cybersecurity" },
-  { title: "Cloud Computing:\n    Optimizing Storage and Scalability", status: "Published", lastEdited: "2 months ago", category: "Cloud Computing" },
+  { title: "Cloud Computing:\n    Optimizing Data and Stability", status: "Published", lastEdited: "4 months ago", category: "Cloud Computing" },
 ];
 
 
